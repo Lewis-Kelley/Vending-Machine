b47077aac32479dfaf9eb5d4aa6e291d1f0e5bb9
Lewis-Kelley <2015412@sluh.org>
2015-04-16 17:29:11 -0500

    Create Inventory.java

---
+++ java/cores/Inventory.java
@@ -0,0 +1,33 @@
+package java.cores;
+
+class Inventory {
+	private final String FILENAME = "filename.txt"; //TODO make actual file
+
+	public Inventory() {
+		//TODO implement
+	}
+	
+	//Returns true if can was removed, returns false if it couldn't find it
+	public boolean removeCan(Soda soda) {
+		//TODO implement
+	}
+	
+	public boolean addSoda(Coordinate coord, Soda soda) {
+		//TODO implement
+	}
+	
+	//Returns true if can was removed, returns false if it couldn't find it
+	private boolean findSoda(Soda soda) {
+		//TODO implement
+	}
+	
+	//Updates file with the current status of the inventory
+	private void updateFile() [
+		//TODO implement
+	}
+	
+	//Reads data from file into current inventory
+	private void readFile() {
+		//TODO implement
+	}
+}
