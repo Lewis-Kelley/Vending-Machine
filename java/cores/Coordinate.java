1c32041a976b8866df2128f18e11ca484ff60baa
Lewis-Kelley <2015412@sluh.org>
2015-04-17 18:07:58 -0500

    Implemented some methods from the Inventory class. Added constructor to Coordinate class.

--- java/cores/Coordinate.java
+++ java/cores/Coordinate.java
@@ -5,4 +5,10 @@ class Coordinate {
 	public double x;
 	public double y;
 	public double z;
+	
+	public Coordinate(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 }
--- java/cores/Inventory.java
+++ java/cores/Inventory.java
@@ -1,24 +1,55 @@
 package java.cores;
 
+import java.io.*;
+
 class Inventory {
 	private final String FILENAME = "filename.txt"; //TODO make actual file
-
+	private final ROWS = 15;
+	private final COLUMN = 5;
+	
+	private FileWriter writer;
+	private FileReader reader;
+	
+	private Soda[][][] inv = new Soda[2][ROWS][COLUMNS];
+	
 	public Inventory() {
-		//TODO implement
+		writer = new FileWriter(FILENAME);
+		reader = new FileReader(FILENAME);
+		
+		for(Soda[][] 2d : inv)
+			for(Soda[] 1d : 2d)
+				for(Soda holder : 1d)
+					holder = null;
 	}
 	
 	//Returns true if can was removed, returns false if it couldn't find it
 	public boolean removeCan(Soda soda) {
-		//TODO implement
+		for(Soda[][] 2d : inv)
+			for(Soda[] 1d : 2d)
+				for(Soda holder : 1d)
+					if(holder == soda) {
+						holder = null;
+						return true;
+					}
+		return false;
 	}
 	
 	public boolean addSoda(Coordinate coord, Soda soda) {
-		//TODO implement
+		if(inv[coord.z][coord.y][coord.x] == null) {
+			inv[coord.z][coord.y][coord.x] = soda;
+			return true;
+		} else{
+			return false;
 	}
 	
-	//Returns true if can was removed, returns false if it couldn't find it
-	private boolean findSoda(Soda soda) {
-		//TODO implement
+	//Returns Coordinate if can was removed, returns null if it couldn't find it
+	private Coordinate findSoda(Soda soda) {
+		for(short out = 0; ct < inv[][].length; out++)
+			for(short mid = 0; ct < inv[].length; mid++)
+				for(short in = 0; ct < inv.length; in++)
+					if(inv[out][mid][in] == soda)
+						return new Coordinate(out, mid, in);
+		return null;
 	}
 	
 	//Updates file with the current status of the inventory
