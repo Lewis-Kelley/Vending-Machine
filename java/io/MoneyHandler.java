0b8a6ce8c0d4557c013fcc4ea2fd3e1272ce7856
Lewis-Kelley <2015412@sluh.org>
2015-05-04 16:05:54 -0500

    Removed all references to coins.

--- java/io/MoneyHandler.java
+++ java/io/MoneyHandler.java
@@ -3,33 +3,26 @@ package java.io;
 class MoneyHandler {
 	private class Money {
 		//Amounts of all the different types 
-		public byte nickels;
-		public byte dimes;
-		public byte quarters;
-		public byte oneDollars;
-		public byte fiveDollars;
-		public byte tenDollars;
-		public byte twentyDollars;
+		public byte ones;
+		public byte fives;
+		public byte tens;
+		public byte twenties;
 		
 		/**
 		 * Simply sets all amounts to 0.
 		 */
 		public Money() {
-			nickels = 0;
-			dimes = 0;
-			quarters = 0;
-			oneDollars = 0;
-			fiveDollars = 0;
-			tenDollars = 0;
-			twentyDollars = 0;
+			ones = 0;
+			fives = 0;
+			tens = 0;
+			twenties = 0;
 		}
 		
 		/**
 		 * Returns the total value of all types money in the object.
 		 */
-		protected float getValue() {
-			return nickels * 0.05f + dimes * 0.1f + quarters * 0.25f
-			       + oneDollars * 1.0f + fiveDollars * 5.0f + tenDollars * 10.0f + twentyDollars * 20.0f;
+		protected short getValue() {
+			return ones * 1 + fives * 5 + tens * 10 + twenties * 20
 		}
 	}
 
@@ -48,13 +41,10 @@ class MoneyHandler {
 	 * Takes one Money object and adds each value to the inputCash object.
 	 */
 	public void addMoney(Money added) {
-		inputCash.nickels += added.nickels;
-		inputCash.dimes += added.dimes;
-		inputCash.quarters += added.quarters;
-		inputCash.oneDollars += added.oneDollars;
-		inputCash.fiveDollars += added.fiveDollars;
-		inputCash.tenDollars += added.tenDollars;
-		inputCash.twentyDollars += added.twentyDollars;
+		inputCash.ones += added.ones;
+		inputCash.fives += added.fives;
+		inputCash.tens += added.tens;
+		inputCash.twenties += added.twenties;
 	}
 	
 	/**
@@ -75,20 +65,20 @@ class MoneyHandler {
 	 * Takes an amount of money input (over the amount) and a goal (the actual needed value)
 	 * and returns a Money object of the amount of money to give in change.
 	 */
-	public Money calcChange(Money input, float goal) {
+	public Money calcChange(Money input, short goal) {
 		Money change = new Money();
-		float difference = goal - inputCash.getValue();
+		short difference = goal - inputCash.getValue();
 		
-		//Number of quarters to return
-		change.quarters += (byte)(difference / 0.25);
+		//Number of tens to return
+		change.tens += (byte)(difference / 10);
 		difference = goal - inputCash.getValue() - change.getValue();
 		
-		//Number of dimes to return
-		change.dimes += (byte)(difference / 0.10);
+		//Number of fives to return
+		change.fives += (byte)(difference / 5);
 		difference = goal - inputCash.getValue() - change.getValue();
 		
-		//Number of nickels to return
-		change.nickels += (byte)(difference / 0.05);
+		//Number of ones to return
+		change.ones += (byte)(difference);
 		
 		return change;
 	}
