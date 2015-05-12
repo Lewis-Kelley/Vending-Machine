52d76e7619e118e3d06d0dd00a85a3a218c945ee
Lewis-Kelley <2015412@sluh.org>
2015-05-12 11:55:20 -0500

    Added unique methods for each state.
    Added descriptions for each state above the methods.

--- java/cores/VendingMachineRunner.java
+++ java/cores/VendingMachineRunner.java
@@ -16,26 +16,84 @@ class VendingMachineRunner {
         while(true) //Infinite loop
             switch(state) {
             case SLEEPING:
-                //TODO implement sleeping state
+                sleeping();
                 break;
             case MENU:
-                //TODO implement menu state
+                menu();
                 break;
             case SHOWING_SODA:
-                //TODO implement showing soda state
+                showingSoda();
                 break;
             case PAYING:
-                //TODO implement paying state
+                paying();
                 break;
             case RETURN_MONEY:
-                //TODO implement returnMoney state
+                returnMoney();
                 break;
             case DELIVERING:
-            	//TODO implement delivering state
+            	delivering();
                 break;
             case DELIVERED:
-                //TODO implement delivered state
+                delivered();
                 break;
             }
+    }
+    
+    /**
+     * Wait for input on touchscreen
+     */
+    private void sleeping() {
+       //TODO implement 
+    }
+    
+    /**
+     * Display menu and wait for selection
+     */
+    private void menu() {
+        //TODO implement
+    }
+    
+    /**
+     * Display picture of soda with a confirmation dialog
+     */
+    private void showingSoda() {
+        //TODO implement
+    }
+    
+    /**
+     * Continue showing the soda picture with a new confirm dialog while the customer pays.
+     * Waits for total price to be inserted and confirm button to be pressed,
+     * cancel button to be pressed, or for a certain amount of time to pass with no input.
+     */
+    private void paying() {
+     //TODO implement
+    }
+     
+    /**
+     * Return input money when customer cancels order.
+     * Display changes to "Come again" screen.
+     */
+    private void returnMoney() {
+        //TODO implement
+    }
+    
+    /**
+     * Changes screen to waiting.
+     * Moves arms to grab can.
+     * Ends when can reaches customer or timeout is reached.
+     * Arm will end up in release position, not home position.
+     * If timeout is reached, return money and display "Needs maintainence" and disable.
+     */
+    private void delivering() {
+        //TODO implement
+    }
+    
+    /**
+     * Changes screen to "Come again" and moves arm back to home position.
+     * Ends when arm reaches home position or when timeout is reached.
+     * If timeout is reached, display "Needs maintainence" and disable.
+     */
+    private void delivered() {
+        //TODO implement
     }
 }
