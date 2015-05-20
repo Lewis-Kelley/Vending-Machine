fa3a24ce006bb6cfe32d5732ed5d7975bd7e81fb
Lewis-Kelley <2015412@sluh.org>
2015-05-20 17:02:50 -0500

    Update Inventory.java
    Fixed all(?) compiler errors.

--- java/cores/Inventory.java
+++ java/cores/Inventory.java
@@ -1,11 +1,9 @@
-package java.cores;
-
 import java.io.*;
 
 class Inventory {
 	private final String FILENAME = "inventory.txt"; //TODO make actual file
 	private final int  ROWS = 15;
-	private final int COLUMN = 5;
+	private final int COLUMNS = 5;
 	
 	private PrintWriter writer;
 	
@@ -15,7 +13,11 @@ class Inventory {
 	 * Initializes writer and each element in the array to null.
 	 */
 	public Inventory() {
-	    writer = new PrintWriter(FILENAME);
+	    try {
+		writer = new PrintWriter(FILENAME);
+	    } catch (Exception e) {
+		System.out.println("Failed to open PrintWriter");
+	    }
 		
 	    for(Soda[][] plane : inv)
 		    for(Soda[] line : plane)
@@ -26,10 +28,10 @@ class Inventory {
 	/**
 	 * Returns true if can was removed, returns false if it couldn't find it
 	 */
-	public boolean removeCan(Soda soda) {
+	public boolean removeSoda(Soda soda) {
 		for(Soda[][] plane : inv)
 			for(Soda[] line : plane)
-				for(Soda holder : line55)
+				for(Soda holder : line)
 					if(holder == soda) {
 						holder = null;
 						return true;
@@ -45,17 +47,17 @@ class Inventory {
 		if(inv[coord.z][coord.y][coord.x] == null) {
 			inv[coord.z][coord.y][coord.x] = soda;
 			return true;
-		} else{
+		} else
 			return false;
 	}
 	
 	/**
 	 * Returns Coordinate if can was found, returns null if it couldn't find it
 	 */
-	private Coordinate findSoda(Soda soda) {
-		for(short out = 0; ct < inv[][].length; out++)
-			for(short mid = 0; ct < inv[].length; mid++)
-				for(short in = 0; ct < inv.length; in++)
+	public Coordinate findSoda(Soda soda) {
+		for(short out = 0; out < inv[0][0].length; out++)
+			for(short mid = 0; mid < inv[0].length; mid++)
+				for(short in = 0; in < inv.length; in++)
 					if(inv[out][mid][in] == soda)
 						return new Coordinate(out, mid, in);
 		return null;
@@ -65,41 +67,52 @@ class Inventory {
 	 * Updates file with the current status of the inventory
 	 */
 	private void updateFile() {
-		writer = new PrintWriter(FILENAME, true); //Creates an object that reads from FILENAME. True = do autoflush
+	    try {
+		writer = new PrintWriter(FILENAME); //Creates an object that reads from FILENAME.
+	    } catch(Exception e) {
+		System.out.println("Failed to open PrintWriter");
+	    }
+	    
+	    for(short column = 0; column < COLUMNS; column++)
+		for(short row = 0; row < ROWS; row++)
+		    writer.print(inv[0][row][column].toString() + " "); //Writes the Soda string and a space
 		
-		for(short column = 0; column < COLUMNS; column++)
-			for(short row = 0; row < ROWS; row++)
-				writer.print(inv[0][rows][columns].toString() + " "); //Writes the Soda string and a space
-		
-		for(short column = 0; column < COLUMNS; column++)
-			for(short row = 0; row < ROWS; row++)
-				writer.print(inv[0][rows][columns].toString() + " "); //Writes the Soda string and a space
+	    for(short column = 0; column < COLUMNS; column++)
+		for(short row = 0; row < ROWS; row++)
+		    writer.print(inv[0][row][column].toString() + " "); //Writes the Soda string and a space
 	}
 	
 	/**
 	 * Reads data from file into current inventory
 	 */
 	private void readFile() {
-		String data = readFile(FILENAME); //gets all the data in the file as one String
-		short lastIndex = 0; //Start of substring
-		short newIndex = 0; //End of substring
+	    String data;
+	    try {
+		data = readFile(FILENAME); //gets all the data in the file as one String
+	    } catch(Exception e) {
+		System.out.println("Failed to read from file");
+		data = null;
+	    }
+
+	    short lastIndex = 0; //Start of substring
+	    short newIndex = 0; //End of substring
 		
-		for(short column = 0; column < COLUMNS; column++)
-			for(short row = 0; row < ROWS; row++) {
-				while(String.charAt(newIndex) != ' ') //Waits to see a space
-					newIndex++;
-				inv[0][row][column] = Soda.valueOf(Soda soda, data.substring(lastIndex, newIndex)); //Puts the substring between the last space and the next one into inv
-				lastIndex = ++newIndex;
-			}
+	    for(short column = 0; column < COLUMNS; column++)
+		for(short row = 0; row < ROWS; row++) {
+		    while(data.charAt(newIndex) != ' ') //Waits to see a space
+			newIndex++;
+		    inv[0][row][column] = Soda.valueOf(data.substring(lastIndex, newIndex)); //Puts the substring between the last space and the next one into inv
+		    lastIndex = ++newIndex;
+		}
 		
-		for(short column = 0; column < COLUMNS; column++)
-			for(short row = 0; row < ROWS; row++) {
-				while(String.charAt(newIndex) != ' ') //Waits to see a space
-					newIndex++;
-				inv[1][row][column] = Soda.valueOf(Soda soda, data.substring(lastIndex, newIndex)); //Puts the substring between the last space and the next one into inv
-				lastIndex = ++newIndex;
-			}
-		
+	    for(short column = 0; column < COLUMNS; column++)
+		for(short row = 0; row < ROWS; row++) {
+		    while(data.charAt(newIndex) != ' ') //Waits to see a space
+			newIndex++;
+		    inv[1][row][column] = Soda.valueOf(data.substring(lastIndex, newIndex)); //Puts the substring between the last space and the next one into inv
+		    lastIndex = ++newIndex;
+		}
+	    
 	}
 	
 	/**
