8de8ebea385fd92b204981d32210d8a167fc405f
Lewis-Kelley <2015412@sluh.org>
2015-05-21 14:57:56 -0500

    Fixed a typo for DIET_MOUNTAIN_DEW

--- java/cores/Soda.java
+++ java/cores/Soda.java
@@ -1,7 +1,7 @@
 enum Soda {
     BRISK_HALF_AND_HALF("Brisk Half-and-Half"), BRISK_RASPBERRY("Brisk Raspberry"), BRISK_LEMONADE("Brisk Lemonade"), BRISK_SWEET_TEA("Brisk Lemonade"),
 	PEPSI("Pepsi"), DIET_PEPSI("Diet Pepsi"), PEPSI_WILD_CHERRY("Pepsi Wild Cherry"), PEPSI_MAX("Pepsi Max"),
-	MOUNTAIN_DEW("Mountain Dew"), DIET_MOUNTIAN_DEW("Diet Mountain Dew"), MOUNTAIN_DEW_CODE_RED("Mountain Dew Code Red"), DIET_MOUNTAIN_DEW_CODE_RED("Diet Mountain Dew Code Red"),
+	MOUNTAIN_DEW("Mountain Dew"), DIET_MOUNTAIN_DEW("Diet Mountain Dew"), MOUNTAIN_DEW_CODE_RED("Mountain Dew Code Red"), DIET_MOUNTAIN_DEW_CODE_RED("Diet Mountain Dew Code Red"),
 	DIET_CRUSH("Diet Crush"), DIET_MUG ("Diet Mug"),
 	EMPTY("");
 
