8c26e29b5638cc75f60c173071309d9d653f9894
Benjamin James <benjamin-james@users.noreply.github.com>
2015-06-01 15:11:56 -0500

    added Makefile and fixed errors

--- javaFiles/actualPrograms/BenSerialListener.java
+++ javaFiles/actualPrograms/BenSerialListener.java
@@ -1,3 +1,6 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
 public class BenSerialListener
 {
     /*
@@ -8,20 +11,20 @@ public class BenSerialListener
      */
 
     private Process p;
-    private java.io.BufferedInputStream in;
-    private java.io.BufferedOutputStream out;
+    private BufferedInputStream in;
+    private BufferedOutputStream out;
 
-    public BenSerialListener(String portname) throws java.io.IOException, InterruptedException
+    public BenSerialListener(String portname) throws IOException, InterruptedException
     {
 	/* Open the tty */
 	Runtime.getRuntime().exec("stty -F " + portname + " cs8 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts").waitFor();
 	p = Runtime.getRuntime().exec("screen " + portname + " 9600");
 	p.waitFor();
-	in = new BufferedInputStream(p.getOutputStream());
-	out = new BufferedOutputStream(p.getInputStream());
+	in = new BufferedInputStream(p.getInputStream());
+	out = new BufferedOutputStream(p.getOutputStream());
     }
 
-    String getLine()
+    String getLine() throws IOException
     {
 	String output = "", s = "";
 	byte buffer[] = new byte[4];
@@ -38,10 +41,10 @@ public class BenSerialListener
      * The letter 'q' is the de facto 'null terminator',
      * so it must be defined that way
      */
-    void println(String input)
+    void println(String input) throws IOException
     {
 	String s = input + "q";
-	byte[] array = s.getBytes(Charset.forName("UTF-8"));
+	byte[] array = s.getBytes();
 	out.write(array);
 	out.flush(); /* make sure it prints the buffer in the stream class */
     }
