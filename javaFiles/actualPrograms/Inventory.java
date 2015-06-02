3ce9b3b8e2ca86e4128d7158f377351a37cd707e
Lewis-Kelley <2015412@sluh.org>
2015-06-02 13:09:31 -0500

    Added a little to the menu function. Added NO_SODA as a state

--- javaFiles/actualPrograms/Inventory.java
+++ javaFiles/actualPrograms/Inventory.java
@@ -81,7 +81,7 @@ class Inventory {
     /**
      * Returns Coordinate if can was found, returns null if it couldn't find it
      */
-    private Coordinate findSoda(Soda soda) {
+    public Coordinate findSoda(Soda soda) {
 	for(short ctZ = 0; ctZ < inv[0][0].length; ctZ++)
 	    for(short ctY = 0; ctY < inv[0].length; ctY++)
 		for(short ctX = 0; ctX < inv.length; ctX++)
--- javaFiles/actualPrograms/VendingMachineRunner.java
+++ javaFiles/actualPrograms/VendingMachineRunner.java
@@ -8,123 +8,148 @@ import java.awt.Font;
 import javax.swing.plaf.FontUIResource;
 
 public class VendingMachineRunner {
-	public enum State { MENU, PAYING, DELIVERING, DELIVERED, DISABLED };
+    public enum State { MENU, NO_SODA, PAYING, DELIVERING, DELIVERED, DISABLED };
 
-	private State state;
-	private Coordinate coord;
-	private Inventory inv;
+    private State state;
+    private Coordinate coord;
+    private Inventory inv;
 
-	private JFrame gui;
-	private VendingGUI vGUI;
+    private JFrame gui;
+    private VendingGUI vGUI;
 
-/*	private BenSerialListener serial; */
+    /*	private BenSerialListener serial; */
 
-	//TODO implement IO handler class
-	//TODO implement systems class
+    //TODO implement IO handler class
+    //TODO implement systems class
 
-	public VendingMachineRunner()
-	{
-		state = State.MENU;
-		coord = null;
-		inv = new Inventory(true);
+    public VendingMachineRunner()
+    {
+	state = State.MENU;
+	coord = null;
+	inv = new Inventory(true);
 
-		gui = new JFrame();
-		vGUI = new VendingGUI();
-		vGUI.setUp();
-		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gui.add(vGUI);
-		gui.pack();
-		gui.setVisible(true);
-/*		serial = new BenSerialListener("/dev/ttyACM0"); */
+	gui = new JFrame();
+	vGUI = new VendingGUI();
+	vGUI.setUp();
+	gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	gui.add(vGUI);
+	gui.pack();
+	gui.setVisible(true);
+	/*		serial = new BenSerialListener("/dev/ttyACM0"); */
+    }
+    
+    public static void main(String[] args) {
+	VendingMachineRunner vmr = new VendingMachineRunner();
+
+	while(true) {
+	    switch(vmr.getState()) {
+	    case MENU:
+		vmr.menu();
+		break;
+	    case NO_SODA:
+		vmr.noSoda();
+		break;
+	    case PAYING:
+		vmr.paying();
+		break;
+	    case DELIVERING:
+		vmr.delivering();
+		break;
+	    case DELIVERED:
+		vmr.delivered();
+		break;
+	    case DISABLED:
+		vmr.disabled();
+		break;
+	    }
+	    try {
+		Thread.sleep(500);
+	    } catch(InterruptedException e) {
+		System.err.println(e);
+	    }
 	}
-	public static void main(String[] args) {
-		VendingMachineRunner vmr = new VendingMachineRunner();
+    }
 
-		while(true) {
-			switch(vmr.getState()) {
-			case MENU:
-				vmr.menu();
-				break;
-			case PAYING:
-				vmr.paying();
-				break;
-			case DELIVERING:
-				vmr.delivering();
-				break;
-			case DELIVERED:
-				vmr.delivered();
-				break;
-			case DISABLED:
-				vmr.disabled();
-				break;
-			}
-			try {
-				Thread.sleep(500);
-			} catch(InterruptedException e) {
-				System.err.println(e);
-			}
-		}
-	}
+    /**
+     * Changes the state to the given value.
+     */
+    public void setState(State nState) {
+	state = nState;
+    }
 
-	/*
-	 * Gets the current state of the machine.
-	 */
-	public State getState()
-	{
-		return state;
-	}
+    /**
+     * Gets the current state of the machine.
+     */
+    public State getState()
+    {
+	return state;
+    }
+    
+    /**
+     * Display welcome screen and wait for touchscreen input.
+     */
+    private void sleeping() {
+	//TODO implement
+    }
 
-	/**
-	 * Display welcome screen and wait for touchscreen input.
-	 */
-	private void sleeping() {
-		//TODO implement
+    /**
+     * Display menu screen.
+     * Ends when option selected or timeout is reached.
+     * If timeout is reached, return to sleeping state.
+     */
+    private void menu() {
+	Soda holder = vGUI.getSoda();
+	
+	if(!holder.equals(Soda.EMPTY)) {
+	    coord = inv.findSoda(holder);
+	    
+	    if(coord == null)
+		state = State.NO_SODA;
+	    else
+		state = State.PAYING;
 	}
+    }
 
-	/**
-	 * Display menu screen.
-	 * Ends when option selected or timeout is reached.
-	 * If timeout is reached, return to sleeping state.
-	 */
-	private void menu() {
-		Soda holder = vGUI.getSoda();
-		if(!holder.equals(Soda.EMPTY))
-			System.out.println("In main, can is: " + holder);
-	}
+    /**
+     * Tell the vGUI to display the Out of Stock card, then return to menu.
+     */
+    private void noSoda() {
+	//TODO implement
+    }
+    
+    /**
+     * Continue showing the soda picture with a new confirm dialog while the customer pays.
+     * Waits for total price to be inserted and confirm button to be pressed,
+     * cancel button to be pressed, or for a certain amount of time to pass with no input.
+     */
+    private void paying() {
+	//TODO implement
+    }
 
-	/*
-	 * Continue showing the soda picture with a new confirm dialog while the customer pays.
-	 * Waits for total price to be inserted and confirm button to be pressed,
-	 * cancel button to be pressed, or for a certain amount of time to pass with no input.
-	 */
-	private void paying() {
-		//TODO implement
-	}
+    /**
+     * Changes screen to waiting.
+     * Moves arms to grab can.
+     * Ends when can reaches customer or timeout is reached.
+     * Arm will end up in release position, not home position.
+     * If timeout is reached, return money and display "Needs maintainence" and disable.
+     */
+    private void delivering() {
+	//TODO implement
+    }
 
-	/*
-	 * Changes screen to waiting.
-	 * Moves arms to grab can.
-	 * Ends when can reaches customer or timeout is reached.
-	 * Arm will end up in release position, not home position.
-	 * If timeout is reached, return money and display "Needs maintainence" and disable.
-	 */
-	private void delivering() {
-		//TODO implement
-	}
+    /**
+     * Changes screen to "Come again" and moves arm back to home position.
+     * Ends when arm reaches home position or when timeout is reached.
+     * If timeout is reached, display "Needs maintainence" and disable.
+     */
+    private void delivered() {
+	//TODO implement
+    }
 
-	/*
-	 * Changes screen to "Come again" and moves arm back to home position.
-	 * Ends when arm reaches home position or when timeout is reached.
-	 * If timeout is reached, display "Needs maintainence" and disable.
-	 */
-	private void delivered() {
-		//TODO implement
-	}
-
-	/*
-	 * Display "Needs maintaince" never leave state until rebooted.
-	 */
-	private void disabled() {
-		//TODO implement
-	}
+    /**
+     * Display "Needs maintaince" never leave state until rebooted.
+     */
+    private void disabled() {
+	//TODO implement
+    }
 }
